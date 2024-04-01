bb89892c179d8244c3df9841620e5756a1ee49a8
soccerbob97 <harsha5431@gmail.com>
2024-04-01 00:27:12 -0400

    Changed main file

--- main.py
+++ main.py
@@ -79,39 +79,29 @@ nba_teams_flipped = {
 }
 
 
-def draw_court(ax=None, color='black', lw=2, outer_lines=False):
+def draw_court(ax=None, color='black', lw=2, outer_lines_element=False):
     # If an axes object isn't provided to plot onto, just get current one
     if ax is None:
         ax = plt.gca()
-    hoop = Circle((0, 0), radius=7.5, linewidth=lw, color=color, fill=False)
-    backboard = Rectangle((-30, -7.5), 60, -1, linewidth=lw, color=color)
-    outer_box = Rectangle((-80, -47.5), 160, 190, linewidth=lw, color=color,
-                          fill=False)
-    inner_box = Rectangle((-60, -47.5), 120, 190, linewidth=lw, color=color,
-                          fill=False)
-    top_free_throw = Arc((0, 142.5), 120, 120, theta1=0, theta2=180,
-                         linewidth=lw, color=color, fill=False)
-    bottom_free_throw = Arc((0, 142.5), 120, 120, theta1=180, theta2=0,
-                            linewidth=lw, color=color, linestyle='dashed')
-    restricted = Arc((0, 0), 80, 80, theta1=0, theta2=180, linewidth=lw,
-                     color=color)
-    corner_three_a = Rectangle((-220, -47.5), 0, 140, linewidth=lw,
-                               color=color)
-    corner_three_b = Rectangle((220, -47.5), 0, 140, linewidth=lw, color=color)
-    three_arc = Arc((0, 0), 475, 475, theta1=22, theta2=158, linewidth=lw,
-                    color=color)
-    center_outer_arc = Arc((0, 422.5), 120, 120, theta1=180, theta2=0,
-                           linewidth=lw, color=color)
-    center_inner_arc = Arc((0, 422.5), 40, 40, theta1=180, theta2=0,
-                           linewidth=lw, color=color)
-    court_elements = [hoop, backboard, outer_box, inner_box, top_free_throw,
-                      bottom_free_throw, restricted, corner_three_a,
-                      corner_three_b, three_arc, center_outer_arc,
-                      center_inner_arc]
-    if outer_lines:
-        outer_lines = Rectangle((-250, -47.5), 500, 470, linewidth=lw,
+    backboard_element = Rectangle((-30, -7.5), 60, -1, linewidth=lw, color=color)
+    outer_box_element = Rectangle((-80, -47.5), 160, 190, linewidth=lw, color=color, fill=False)
+    inner_box_element = Rectangle((-60, -47.5), 120, 190, linewidth=lw, color=color, fill=False)
+    top_free_throw_element = Arc((0, 142.5), 120, 120, theta1=0, theta2=180, linewidth=lw, color=color, fill=False)
+    bottom_free_throw_element = Arc((0, 142.5), 120, 120, theta1=180, theta2=0, linewidth=lw, color=color, linestyle='dashed')
+    restricted_element = Arc((0, 0), 80, 80, theta1=0, theta2=180, linewidth=lw, color=color)
+    center_outer_arc_element = Arc((0, 422.5), 120, 120, theta1=180, theta2=0, linewidth=lw, color=color)
+    center_inner_arc_element = Arc((0, 422.5), 40, 40, theta1=180, theta2=0, linewidth=lw, color=color)
+    corner_three_a_element = Rectangle((-220, -47.5), 0, 140, linewidth=lw, color=color)
+    corner_three_b_element = Rectangle((220, -47.5), 0, 140, linewidth=lw, color=color)
+    three_arc_element = Arc((0, 0), 475, 475, theta1=22, theta2=158, linewidth=lw, color=color)
+    hoop_element = Circle((0, 0), radius=7.5, linewidth=lw, color=color, fill=False)
+    court_elements = [backboard_element, center_outer_arc_element, center_inner_arc_element,
+                      top_free_throw_element, bottom_free_throw_element, restricted_element, corner_three_a_element, 
+                      outer_box_element, inner_box_element, corner_three_b_element, three_arc_element, hoop_element]
+    if outer_lines_element:
+        outer_lines_element = Rectangle((-250, -47.5), 500, 470, linewidth=lw,
                                 color=color, fill=False)
-        court_elements.append(outer_lines)
+        court_elements.append(outer_lines_element)
     for element in court_elements:
         ax.add_patch(element)
     return ax
@@ -152,7 +142,7 @@ def plotLocations(df):
     sns.set_color_codes()
     fig = plt.figure(figsize=(12,11))
     plt.scatter(df.LOC_X, df.LOC_Y)
-    draw_court(outer_lines=True)
+    draw_court(outer_lines_element=True)
     plt.xlim(-250,250)
     plt.ylim(422.5, -15)
     st.pyplot(fig)
