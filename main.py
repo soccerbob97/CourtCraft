39857f7ee6a0369bc123e1852bebb90729f62974
Ajay Gill <ajaisgll@gmail.com>
2024-04-18 23:17:55 -0400

    Added KDE and HEX plot feature and marked shots as made or missed on base plot

--- main.py
+++ main.py
@@ -125,28 +125,41 @@ def getAllData():
     print("number of rows ", df.shape[0])
     return df
     
-def plotPlayerShotLocations(df, player_name):
-    player_df = df[df["PLAYER_NAME"] == player_name].copy()
-    player_df.loc[:,"LOC_X"] = player_df.loc[:,"LOC_X"] * 10 
-    player_df.loc[:,"LOC_Y"] = player_df.loc[:,"LOC_Y"] * 10 - 45
-    plotLocations(player_df)
-
-def plotTeamShotLocations(df, year, team_name, game_id):
-    team_df = df[(df["TEAM_NAME"] == team_name) & (df["SEASON_1"] == int(year)) & (df["GAME_ID"] == int(game_id))].copy()
-    team_df.loc[:, "LOC_X"] = team_df.loc[:, "LOC_X"] * 10 
-    team_df.loc[:, "LOC_Y"] = team_df.loc[:, "LOC_Y"] * 10 - 45
-    plotLocations(team_df)
-
-def plotLocations(df):  
+def plotShotLocations(df, plot_type):
     sns.set_style("white")
     sns.set_color_codes()
     fig = plt.figure(figsize=(12,11))
-    plt.scatter(df.LOC_X, df.LOC_Y)
+    if plot_type == 'Simple':
+        made_shots = df[df['SHOT_MADE'] == True]
+        missed_shots = df[df['SHOT_MADE'] == False]
+        plt.scatter(made_shots['LOC_X'], made_shots['LOC_Y'], color='green', label='Made')
+        plt.scatter(missed_shots['LOC_X'], missed_shots['LOC_Y'], color='red', label='Missed')
+    elif plot_type == 'KDE':
+        sns.kdeplot(x=df['LOC_X'], y=df['LOC_Y'], cmap=plt.cm.YlOrRd_r, shade=True, shade_lowest=False, cbar=False)
+    else:
+        plt.hexbin(df['LOC_X'], df['LOC_Y'], gridsize=28, cmap='YlOrRd', bins='log')
     draw_court(outer_lines_element=True)
     plt.xlim(-250,250)
     plt.ylim(422.5, -15)
+    plt.xlabel('')
+    plt.ylabel('')
+    if plot_type == 'Simple':
+        plt.legend()
     st.pyplot(fig)
 
+def plotPlayerShotLocations(df, player_name, plot_type):
+    player_df = df[df["PLAYER_NAME"] == player_name].copy()
+    player_df['LOC_X'] *= 10 
+    player_df['LOC_Y'] = player_df['LOC_Y'] * 10 - 45
+    plotShotLocations(player_df, plot_type)
+
+def plotTeamShotLocations(df, year, team_name, game_id, plot_type):
+    team_df = df[(df["TEAM_NAME"] == team_name) & (df["SEASON_1"] == int(year)) & (df["GAME_ID"] == int(game_id))].copy()
+    team_df['LOC_X'] *= 10 
+    team_df['LOC_Y'] = team_df['LOC_Y'] * 10 - 45
+    plotShotLocations(team_df, plot_type)
+
+    
 def main():
     st.title('NBA HeatMaps')
     all_data = getAllData()
@@ -158,13 +171,25 @@ def main():
         years = [str(i) for i in range(2004, 2024)]
         years.insert(0, "All years")
         year_option = st.selectbox('Select a year', options=years)
+        plot_option = st.selectbox('Select Plot Type', options=['Simple Shot Chart', 'KDE Shot Chart', 'HEX Shot Chart'])
         btn = st.button("Create Heat Map for Player")
         if btn:
             if year_option == "All years":
-                plotPlayerShotLocations(all_data, player_option)
+                if plot_option == "Simple Shot Chart":
+                    plotPlayerShotLocations(all_data, player_option, "Simple")
+                elif plot_option == "KDE Shot Chart":
+                    plotPlayerShotLocations(all_data, player_option, "KDE")
+                else:
+                    plotPlayerShotLocations(all_data, player_option, "HEX")
             else:
                 df = getDataframeForYear(year_option)
-                plotPlayerShotLocations(df, player_option)
+                if year_option == "All years":
+                    if plot_option == "Simple Shot Chart":
+                        plotPlayerShotLocations(df, player_option, "Simple")
+                    elif plot_option == "KDE Shot Chart":
+                        plotPlayerShotLocations(df, player_option, "KDE")
+                    else:
+                        plotPlayerShotLocations(df, player_option, "HEX")
     elif option == 'Team':
         team_option = st.selectbox('Select a Team', options=list(nba_teams.values()))
         year_option = st.selectbox('Select a year', options=[str(i) for i in range(2004, 2024)])
@@ -179,10 +204,16 @@ def main():
         
         selected_game_ids = filtered_data[((filtered_data['HOME_TEAM'].map(nba_teams) == team_option) | (filtered_data['AWAY_TEAM'].map(nba_teams) == team_option)) & (filtered_data['GAME_DATE'] == selected_game_date)]['GAME_ID'].unique()
         
+        plot_option = st.selectbox('Select Plot Type', options=['Simple Shot Chart', 'KDE Shot Chart', 'HEX Shot Chart'])
         btn = st.button("Create Heat Map for Team")
         if btn:
             for game_id in selected_game_ids:
-                plotTeamShotLocations(all_data, year_option, team_option, game_id)
+                if plot_option == "Simple Shot Chart":
+                    plotTeamShotLocations(all_data, year_option, team_option, game_id, "Simple")
+                elif plot_option == "KDE Shot Chart":
+                    plotTeamShotLocations(all_data, year_option, team_option, game_id, "KDE")
+                else:
+                    plotTeamShotLocations(all_data, year_option, team_option, game_id, "HEX")
 
 if __name__ == "__main__":
     main()
